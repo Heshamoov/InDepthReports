677d8306ef6bea3dc8269584cd56bb98b5f9add0
Heshamoov <Heshamoov90@gmail.com>
2019-02-28 13:17:13 +0400

    Sorting the order of sections.

--- sqldb/distinctSection.php
+++ sqldb/distinctSection.php
@@ -1,25 +1,25 @@
 <?php
-        $servername = "localhost";          $username = "reports2018";
-        $password = "Indepth2018";        $DB = "fedena_pro";
+    $servername = "localhost";          $username = "reports2018";
+    $password = "Indepth2018";        $DB = "fedena_pro";
 
-        $conn = new mysqli($servername, $username, $password, $DB);
+    $conn = new mysqli($servername, $username, $password, $DB);
 
-        if ($conn->connect_error)
-                die("Connection failed: " . $conn->connect_error . "\n");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error . "\n");
+    }
 
-        $grade = $_REQUEST["grade"];
-        $sql = "SELECT DISTINCT batches.name name FROM batches\n "
-                . "INNER JOIN courses\n"
-                . "ON batches.course_id = courses.id\n "
-                . "WHERE courses.course_name = '$grade' "
-                . "AND batches.is_deleted = 0 "
-                . "ORDER By batches.academic_year_id DESC, batches.name ASC";
-//                echo $sql;
+    $grade = $_REQUEST["grade"];
+    $sql = "SELECT DISTINCT batches.name name FROM batches\n "
+         . "INNER JOIN courses\n"
+         . "ON batches.course_id = courses.id\n "
+         . "WHERE courses.course_name = '$grade' "
+         . "AND batches.is_deleted = 0 "
+         . "ORDER By batches.academic_year_id DESC, batches.name ASC";
+
+//       echo $sql;        
         
-        
+    $result = $conn->query($sql);
+    while($row = mysqli_fetch_array($result))
+    { echo $row['name'] . "?";}
 
-        $result = $conn->query($sql);
-        while($row = mysqli_fetch_array($result))
-        { echo $row['name'] . "?";}
-        
-        $conn->close();
+    $conn->close();
